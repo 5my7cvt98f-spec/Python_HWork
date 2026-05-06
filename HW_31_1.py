cd76cb8234d4deb498fb8ed2eab3248952fa19f4
Svitlana_L <5my7cvt98f@privaterelay.appleid.com>
2026-05-06 19:28:28 +0200

    Add files via upload

---
+++ HW_31_1.py
@@ -0,0 +1,7 @@
+# Извлечение дат
+# Реализуйте программу, которая должна:
+# Найти в тексте все даты в форматах DD/MM/YYYY, DD-MM-YYYY и DD.MM.YYYY.
+
+import re
+text = "The events N 123456 happened on 15/03/2025, 01.12.2024 and 09-09-2023. Deadline: 28/02/2022."
+print(re.findall(r"\b\d{2}[\/\-.]\d{2}[\/\-.]\d{4}\b", text))
