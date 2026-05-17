3163adf0c624d6a4a7c6c703a449d5797ba9a719
Svitlana_L <5my7cvt98f@privaterelay.appleid.com>
2026-05-17 14:33:42 +0200

    Add files via upload

--- HW_33.py
+++ HW_33.py
@@ -1,33 +1,34 @@
 class Person:
     def __init__(self, name):
         self.name = name
-        
+
     def introduce(self):
         print(f"Hello, my name is {self.name}")
-        
+
+
 class Student(Person):
     def __init__(self, name, course):
-        super().__init__(name)  # вызываем родительский __init__
+        super().__init__(name)
         self.course = course
-   
+
     def introduce(self):
-        print(f"I'm on course {self.course}")   
+        print(f"I'm on course {self.course}")
+
 
 class Teacher(Person):
     def __init__(self, name, subject):
-        super().__init__(name)  # вызываем родительский __init__
+        super().__init__(name)
         self.subject = subject
-   
+
     def introduce(self):
-        print(f"Hello, I am professor  {self.name}. \nMy subject is {self.subject}")   
-     
+        print(f"Hello, I am professor  {self.name}. \nMy subject is {self.subject}")
+
+
 user = Person("Alice")
 user.introduce()
-# user1 = Student("Alice",2)
-# user1.introduce()
 
-staff = [Student("Alice",2), Teacher("Bob", "Mathematics")]
+staff = [Student("Alice", 2), Teacher("Bob", "Mathematics")]
 
 for person in staff:
-    person.introduce() 
+    person.introduce()
     print("-----")
