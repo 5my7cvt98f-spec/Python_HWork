cc51b676e6ddb330b0d3c58dcd79b4d8007ba4dd
Svitlana_L <5my7cvt98f@privaterelay.appleid.com>
2026-05-17 14:31:08 +0200

    Add files via upload

--- HW_31_1.py
+++ HW_31_1.py
@@ -3,6 +3,6 @@
 # Найти в тексте все даты в форматах DD/MM/YYYY, DD-MM-YYYY и DD.MM.YYYY.
 
 import re
+
 text = "The events N 123456 happened on 15/03/2025, 01.12.2024 and 09-09-2023. Deadline: 28/02/2022."
-print(re.findall(r"\d{2}[/.-]\d{2}[/.-]\d{4}", text))
-# print(*re.findall(r"\d{2}[/.-]\d{2}[/.-]\d{4}", text), sep='\n')
+print(*re.findall(r"\d{2}[/.-]\d{2}[/.-]\d{4}", text), sep="\n")
